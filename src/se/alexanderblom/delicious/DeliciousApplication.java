7b6ab5e4b5b640ed50817f07188fd7778cb981ce
Alexander Blom <me@alexanderblom.se>
2012-05-21 22:28:26 +0200

    Enabled strict mode

--- src/se/alexanderblom/delicious/DeliciousApplication.java
+++ src/se/alexanderblom/delicious/DeliciousApplication.java
@@ -2,6 +2,7 @@ package se.alexanderblom.delicious;
 
 import android.app.Application;
 import android.graphics.drawable.Drawable;
+import android.os.StrictMode;
 
 public class DeliciousApplication extends Application {
 	private static Drawable errorDrawable;
@@ -16,6 +17,11 @@ public class DeliciousApplication extends Application {
 
 		errorDrawable = getResources().getDrawable(R.drawable.ic_editext_error);
 		errorDrawable.setBounds(0, 0, errorDimen, errorDimen);
+		
+		if (BuildConfig.DEBUG) {
+			StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().build());
+			StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().build());
+		}
 	}
 
 	public static Drawable getErrorDrawable() {
