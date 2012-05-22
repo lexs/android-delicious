2fa1cccd2339db3c34ddc56b295c1732ecd78e95
Alexander Blom <me@alexanderblom.se>
2012-05-22 11:27:27 +0200

    Use intrinsic size

--- src/se/alexanderblom/delicious/DeliciousApplication.java
+++ src/se/alexanderblom/delicious/DeliciousApplication.java
@@ -13,10 +13,8 @@ public class DeliciousApplication extends Application {
 		
 		System.setProperty("http.agent", Constants.HTTP_AGENT);
 
-		int errorDimen = getResources().getDimensionPixelSize(R.dimen.ic_editext_error_dimen);
-
 		errorDrawable = getResources().getDrawable(R.drawable.ic_editext_error);
-		errorDrawable.setBounds(0, 0, errorDimen, errorDimen);
+		errorDrawable.setBounds(0, 0, errorDrawable.getIntrinsicWidth(), errorDrawable.getIntrinsicHeight());
 		
 		if (BuildConfig.DEBUG) {
 			StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().build());
